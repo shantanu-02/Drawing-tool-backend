01b1d2bee946aba877c512608edde005225d398a
unknown <shantanupawar101@gmail.com>
2024-02-17 20:52:40 +0530

    Whitelist Client

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+const isDev = app.settings.env === "development";
+const URL = isDev
+  ? "http://localhost:3000"
+  : "https://drawing-tool-seven.vercel.app";
+app.use(cors({ origin: URL }));
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: "http://localhost:3000" });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
   console.log("server connected");
 
-  socket.on('beginPath', (arg) => {
-    socket.broadcast.emit('beginPath', arg)
-  })
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
 
-  socket.on('drawLine', (arg) => {
-    socket.broadcast.emit('drawLine', arg)
-  })
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  });
 
-  socket.on('changeConfig', (arg) => {
-    socket.broadcast.emit('changeConfig', arg)
-  })
+  socket.on("changeConfig", (arg) => {
+    socket.broadcast.emit("changeConfig", arg);
+  });
 });
 
 httpServer.listen(5000);
